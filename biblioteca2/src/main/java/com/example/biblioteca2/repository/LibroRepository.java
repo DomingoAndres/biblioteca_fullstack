0d3e19c39790978dd74c177aab008b79cb8c7aab
Domingo Velazquez <domingoandresv98@gmail.com>
2025-04-12 12:30:55 -0400

    Se agrego funcion de eliminar libro

--- biblioteca2/src/main/java/com/example/biblioteca2/repository/LibroRepository.java
+++ biblioteca2/src/main/java/com/example/biblioteca2/repository/LibroRepository.java
@@ -1,6 +1,6 @@
 package com.example.biblioteca2.repository;
 
-import com.example.biblioteca2.model.Libro;
+import com.example.biblioteca2.model.LibroModel;
 
 import jakarta.annotation.PostConstruct;
 
@@ -11,37 +11,52 @@ import java.util.ArrayList;
 
 public class LibroRepository {
 
-    private List<Libro> listaLibros = new ArrayList<>();
+    private List<LibroModel> listaLibros = new ArrayList<>();
 
     @PostConstruct
     public void init(){
 
-        listaLibros.add(new Libro(1, "978-0134685991", "Effective Java", "Addison-Wesley", 2018, "Joshua Bloch"));
+        listaLibros.add(new LibroModel(1, "978-0134685991", "Effective Java", "Addison-Wesley", 2018, "Joshua Bloch"));
 
-        listaLibros.add(new Libro(2, "978-1617294945", "Spring in Action", "Manning", 2020, "Craig Walls"));
+        listaLibros.add(new LibroModel(2, "978-1617294945", "Spring in Action", "Manning", 2020, "Craig Walls"));
 
-        listaLibros.add(new Libro(3, "978-1491950357", "Designing Data-Intensive Applications", "O'Reilly Media", 2017, "Martin Kleppmann"));
+        listaLibros.add(new LibroModel(3, "978-1491950357", "Designing Data-Intensive Applications", "O'Reilly Media", 2017, "Martin Kleppmann"));
 
-        listaLibros.add(new Libro(4, "978-0132350884", "Clean Code", "Prentice Hall", 2008, "Robert C. Martin"));
+        listaLibros.add(new LibroModel(4, "978-0132350884", "Clean Code", "Prentice Hall", 2008, "Robert C. Martin"));
     }
 
-    public List<Libro> obtenerLibros(){
+    public List<LibroModel> obtenerLibros(){
         return listaLibros;
     }
 
-    public Libro buscarLibroPorId(int id){
+    public LibroModel buscarLibroPorId(int id){
         
-        for(Libro libro : listaLibros){
+        for(LibroModel libro : listaLibros){
             if(libro.getId() == id) return libro;
         }
         return null;
     }
 
     //buscar libro por isbn
+    public LibroModel buscarLibroPorIsbn(String isbn){  
+        
+        for(LibroModel libro : listaLibros){
+            if(libro.getIsbn() == isbn) return libro;
+        }
+        return null;
+    }
 
     //buscar por autor
+    public LibroModel buscarLibroPorAutor(String autor){
+        
+        for(LibroModel libro : listaLibros){
+            if(libro.getAutor() == autor) return libro;
+        }
+        return null;
+    }
 
-    public Libro actualizar(Libro lib){
+
+    public LibroModel actualizar(LibroModel lib){
         
         int id = 0;
         int idPosicion = 0;
@@ -52,7 +67,41 @@ public class LibroRepository {
                 idPosicion = i;
             }
         }
-        return null;
+        LibroModel libro1 = new LibroModel();
+        libro1.setId(id);
+        libro1.setTitulo(lib.getTitulo());
+        libro1.setAutor(lib.getAutor());
+        libro1.setFechaPublicacion(lib.getFechaPublicacion());
+        libro1.setEditorial(lib.getEditorial());
+        libro1.setIsbn(lib.getIsbn());
+
+        listaLibros.set(idPosicion, libro1);
+        return libro1;
+    }
+
+
+    public void eliminar(int id){
+        //Altyernativa 1
+
+        LibroModel libro = buscarLibroPorId(id);
+        if(libro != null) listaLibros.remove(libro);
+
+        //Alternativa 2
+
+        int idPosicion = 0;
+        for(int i=0;i<listaLibros.size();i++){
+            if(listaLibros.get(i).getId() == id){
+                idPosicion = i;
+                break;
+            }
+        }
+        if(idPosicion > 0){
+            listaLibros.remove(idPosicion);
+        }
+
+        //Alternativa 3
+
+        listaLibros.removeIf((x) -> x.getId() == id);
     }
 
 }
